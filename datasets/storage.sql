e4b1d51f9bcd5d96fa04b7ef5d7ce4957fa402f2
chronox <xonorhc@proton.me>
2026-03-15 17:49:20 -0400

    feat(datasets): add create table to the storage

---
+++ datasets/storage.sql
@@ -0,0 +1,29 @@
+CREATE TABLE IF NOT EXISTS storage (
+    object_id serial PRIMARY KEY, -- Unique identifier for each feature in the layer
+    asset_type smallint REFERENCES asset_type_water_device_storage (id) NOT NULL DEFAULT 0, -- Categorization of the type of water distribution asset
+    asset_id varchar(64), -- Identifier assigned to the asset for tracking purposes
+    height numeric, -- Vertical measurement of the storage asset
+    width numeric, -- Horizontal measurement of the storage asset
+    volume numeric, -- Capacity or volume of the storage asset
+    install_date date, -- Date when the asset was installed
+    lifecycle_status smallint REFERENCES lifecycle (1) NOT NULL DEFAULT 8, -- Current stage or condition of the asset in its lifecycle
+    inservice_date date, -- Date when the asset was put into active service
+    retired_date date, -- Date when the asset was retired
+    notes varchar(2000), -- Additional information or comments about the asset
+    last_maint date, -- Date of the most recent maintenance performed on the asset
+    owned_by smallint REFERENCES asset_owner (id) DEFAULT 1, -- Entity or organization that owns the asset
+    main_by smallint REFERENCES asset_manager (id) DEFAULT 1, -- Entity or organization responsible for maintaining the asset
+    spatial_source smallint REFERENCES spatial_source (id) DEFAULT 0, -- Describe the source of spatial data for the assets location
+    spatial_confidence smallint REFERENCES spatial_confidence (id) DEFAULT 0, -- Level of confidence in the accuracy of the spatial data
+    global_id serial PRIMARY KEY, -- Globally unique identifier fo the feature
+    latitude real, -- Latitude coordinate of the asset
+    longitude real, -- Longitude coordinate of the asset
+    altitude real, -- Altitude of the assets location above sea level
+    created_user varchar(255), -- User who created the feature
+    created_date timestamp, -- Date and time when the feature was created
+    last_edited_user varchar(255), -- Tracks the user who last edited the record
+    last_edited_date timestamp, -- Date and time of the most recent modification to the record
+    shape geometry(point, 4326) -- Geometric representation of the feature
+);
+
+CREATE INDEX ON storage USING gist (shape);
