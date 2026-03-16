28762500bf6901835bc4c0210648462dbde5357f
xonorhc <xonorhc@proton.me>
2026-03-16 17:05:45 -0400

    feat(domains): add data definition for layer "asset_type_water_device_system_meter"

---
+++ domains/asset_type_water_device_system_meter.sql
@@ -0,0 +1,13 @@
+CREATE TABLE IF NOT EXISTS asset_type_water_device_system_meter (
+    code smallserial,
+    description varchar(64) NOT NULL,
+    details varchar(255)
+);
+
+INSERT INTO asset_type_water_device_system_meter (description)
+VALUES
+    ("Unknow"),
+    ("Bulk"),
+    ("DMA"),
+    ("Emergency Connection"),
+    ("Pump Station");
