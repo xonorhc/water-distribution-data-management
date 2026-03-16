5e204b09ebb444a3bc76d9db95fc5b8e79c1cb01
xonorhc <xonorhc@proton.me>
2026-03-16 17:05:41 -0400

    feat(domains): add data definition for layer "asset_type_water_device_supply_welltype"

---
+++ domains/asset_type_water_device_supply_welltype.sql
@@ -0,0 +1,12 @@
+CREATE TABLE IF NOT EXISTS asset_type_water_device_supply_welltype (
+    code smallserial,
+    description varchar(64) NOT NULL,
+    details varchar(255)
+);
+
+INSERT INTO asset_type_water_device_supply_welltype (description)
+VALUES
+    ("Unknow"),
+    ("Water"),
+    ("Monitoring"),
+    ("Injection");
