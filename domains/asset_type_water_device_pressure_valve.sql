42aa4909e719e15c1032bd3536909a2bdde06dae
xonorhc <xonorhc@proton.me>
2026-03-16 17:05:05 -0400

    feat(domains): add data definition for layer "asset_type_water_device_pressure_valve"

---
+++ domains/asset_type_water_device_pressure_valve.sql
@@ -0,0 +1,17 @@
+CREATE TABLE IF NOT EXISTS asset_type_water_device_pressure_valve (
+    code smallserial,
+    description varchar(64) NOT NULL,
+    details varchar(255)
+);
+
+INSERT INTO asset_type_water_device_pressure_valve (description)
+VALUES
+    ("Unknow"),
+    ("Air and Vacuum"),
+    ("Air Release"),
+    ("Combination Air"),
+    ("Pressure Limiting"),
+    ("Pressure Reducing"),
+    ("Pressure Release"),
+    ("Pressure Sustaining"),
+    ("Surge");
