b5c20340ceabb60c75d4369723518ea358f74c07
xonorhc <xonorhc@proton.me>
2026-03-16 17:04:41 -0400

    feat(domains): add data definition for layer "asset_type_water_assembly_backflow"

---
+++ domains/asset_type_water_assembly_backflow.sql
@@ -0,0 +1,13 @@
+-- FIX: Change this name
+CREATE TABLE IF NOT EXISTS asset_type_water_assembly_backflow (
+    code smallserial,
+    description varchar(64) NOT NULL,
+    details varchar(255)
+);
+
+INSERT INTO asset_type_water_assembly_backflow (description)
+VALUES
+    ("Unknow"),
+    ("Double Check Detector"),
+    ("Double Check Valve"),
+    ("Reduced Pressure Detector");
