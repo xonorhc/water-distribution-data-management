16808dcf5741cc0200430c2c32dfa20fa941c130
xonorhc <xonorhc@proton.me>
2026-03-16 17:05:50 -0400

    feat(domains): add data definition for layer "asset_type_water_device_system_valve"

---
+++ domains/asset_type_water_device_system_valve.sql
@@ -0,0 +1,15 @@
+CREATE TABLE IF NOT EXISTS asset_type_water_device_system_valve (
+    code smallserial,
+    description varchar(64) NOT NULL,
+    details varchar(255)
+);
+
+INSERT INTO asset_type_water_device_system_valve (description)
+VALUES
+    ("Unknow"),
+    ("Bypass"),
+    ("Interconnect"),
+    ("Isolation Station"),
+    ("Pressure Zone"),
+    ("Supply"),
+    ("System");
