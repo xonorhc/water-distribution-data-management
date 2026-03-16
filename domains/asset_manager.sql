b98ac4e78da3426ef1e4f42346f4ffd516e1bd4a
xonorhc <xonorhc@proton.me>
2026-03-16 17:03:14 -0400

    feat(domains): add data definition for layer "asset_manager"

---
+++ domains/asset_manager.sql
@@ -0,0 +1,11 @@
+CREATE TABLE IF NOT EXISTS asset_manager (
+    code smallserial,
+    description varchar(64) NOT NULL,
+    details varchar(255)
+);
+
+INSERT INTO asset_manager (description)
+VALUES
+    ("Our Agency"),
+    ("Private"),
+    ("Other");
