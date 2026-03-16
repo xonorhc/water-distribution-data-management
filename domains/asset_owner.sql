1452fe0066657c08c6f69dd17ec44b71a4301542
xonorhc <xonorhc@proton.me>
2026-03-16 17:03:58 -0400

    feat(domains): add data definition for layer "asset_owner"

---
+++ domains/asset_owner.sql
@@ -0,0 +1,11 @@
+CREATE TABLE IF NOT EXISTS asset_owner (
+    code smallserial,
+    description varchar(64) NOT NULL,
+    details varchar(255)
+);
+
+INSERT INTO asset_owner (description)
+VALUES
+    ("Our Agency"),
+    ("Private"),
+    ("Other");
