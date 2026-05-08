63dd175af43cf7d06323a10f77c5d5418b14fcac
xonorhc <xonorhc@proton.me>
2026-05-07 21:27:28 -0400

    feat(setup): add variable to set SRID

--- db/03_datasets/01_punctual_asset.sql
+++ db/03_datasets/01_punctual_asset.sql
@@ -1,6 +1,8 @@
 -- TABLE: punctual_asset
 CREATE TABLE IF NOT EXISTS watersystem.punctual_asset (
-    shape geometry(POINTZ, 4326),
+    latitude numeric(9, 7) GENERATED ALWAYS AS (ST_Y (ST_TRANSFORM (shape, 4326))) STORED,
+    longitude numeric(10, 7) GENERATED ALWAYS AS (ST_X (ST_TRANSFORM (shape, 4326))) STORED,
+    shape geometry(point, :SRID) NOT NULL,
     symbol_rotation smallint DEFAULT 0,
     CHECK (symbol_rotation BETWEEN 0 AND 359)
 )
