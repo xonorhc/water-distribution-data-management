34d6a04bc76a2e2361e3a888b9bda7b53a0694ca
xonorhc <xonorhc@proton.me>
2026-05-16 16:22:21 -0400

    fix(db): incorrect column name

--- db/05_functions/01_functions.sql
+++ db/05_functions/01_functions.sql
@@ -18,8 +18,8 @@ CREATE OR REPLACE FUNCTION last_edited_user ()
     COST 100 VOLATILE NOT LEAKPROOF
     AS $body$
 BEGIN
-    NEW.created_user := CURRENT_USER;
-    NEW.created_date := CURRENT_TIMESTAMP;
+    NEW.last_edited_user := CURRENT_USER;
+    NEW.last_edited_date := CURRENT_TIMESTAMP;
     RETURN new;
 END;
 $body$;
